bc59ee0d25690f659604cb520ceea7e47e9606cb
Love Babbar <lovebabbar@192.168.0.149>
2021-12-12 15:25:23 +0530

    Insertion sort Code Added

---
+++ Lecture18: Insertion Sort/insertionSort.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+using namespace std;
+
+void insertionSort(int n, vector<int> &arr){
+    
+    for(int i = 1; i<n; i++) {
+        int temp = arr[i];
+        int j = i-1;
+        for(; j>=0; j--) {
+            
+            if(arr[j] > temp) {
+                //shift
+                arr[j+1] = arr[j];
+            }
+            else { // ruk jao
+                break;
+            }
+            
+        }
+        //copy temp value
+        arr[j+1] = temp;  
+    } 
+}
