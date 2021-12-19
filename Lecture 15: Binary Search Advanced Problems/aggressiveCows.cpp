cd6cf4c3c9290cf306d4ed83cfa679dad6b0e3c9
mohd-aajam <79818414+mohd-aajam@users.noreply.github.com>
2021-12-19 10:58:45 +0530

    Update aggressiveCows.cpp

--- Lecture 15: Binary Search Advanced Problems/aggressiveCows.cpp
+++ Lecture 15: Binary Search Advanced Problems/aggressiveCows.cpp
@@ -1,12 +1,9 @@
-#include<vector>
-using namespace std;
-
-bool isPossible(vector<int> &stalls, int k, int mid) {
+bool isPossible(vector<int> &stalls, int k, int mid, int n) {
     
     int cowCount = 1;
     int lastPos = stalls[0];
     
-    for(int i=0; i<stalls.size(); i++ ){
+    for(int i=0; i<n; i++ ){
         
         if(stalls[i]-lastPos >= mid){
             cowCount++;
@@ -23,18 +20,14 @@ bool isPossible(vector<int> &stalls, int k, int mid) {
 int aggressiveCows(vector<int> &stalls, int k)
 {
     sort(stalls.begin(), stalls.end());
-    
    	int s = 0;
-    int maxi = -1;
-    for(int i=0; i<stalls.size(); i++) {
-        maxi = max(maxi, stalls[i]);
-    }
-    int e = maxi;
+    int n = stalls.size();
+    int e=stalls[n-1];
     int ans = -1;
     int mid = s + (e-s)/2;
     
     while(s<=e) {
-        if(isPossible(stalls, k, mid)) {
+        if(isPossible(stalls, k, mid, n)) {
             ans = mid;
             s = mid + 1;
         }
