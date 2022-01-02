33bc2f0de80b11e43dae96675192bb7b4430bcb9
Deepak Kumar <85002425+DeepakKumar-1@users.noreply.github.com>
2022-01-03 01:37:36 +0530

    Create NegativeNumberRepresentation.java

---
+++ Lecture 5: Bitwise Operators, For Loops, Operator Precedence & Variable Scoping/NegativeNumberRepresentation.java
@@ -0,0 +1,9 @@
+package BitManipulation;
+
+public class NegativeNumberRepresentation {
+    // Negative Number Representation in Binary
+    public static void main(String [] args){
+        int a = 10;
+        System.out.println( - a + " in Binary: " + Integer.toBinaryString(-a));
+    }
+}
