af9084ae0554402b399bfe30a26e5edae22bd730
Kaustav Banerjee <35100716+kaustav1810@users.noreply.github.com>
2022-01-03 16:20:12 +0530

    Create duplicatesInArray.java

---
+++ Lecture 10: Solving LeetCode Arrays/duplicatesInArray.java
@@ -0,0 +1,16 @@
+
+public static int findDuplicate(ArrayList<Integer> arr) 
+{
+    int ans = 0;
+    
+    //XOR ing all array elements
+    for(int i = 0; i<arr.size(); i++ ) {
+    	ans = ans^arr.get(i);
+    }
+	
+    //XOR [1, n-1]
+    for(int i = 1; i<arr.size();i++ ) {
+    	ans = ans^i;
+    }
+    return ans;
+}
