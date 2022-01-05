8e298bcc9c083c12e2c57947b52727783570632d
Mohit Rathore <80819747+mr-mohit-rathore@users.noreply.github.com>
2022-01-06 00:43:31 +0530

    Update Fibbonacci.cpp
    This is a complete code by using main function means int main(). And there is a function that fibbonacci(int n). that call itself so that's why it is called recursive function and this process is called recursion.

--- Lecture32: Recursion Day2/Fibbonacci.cpp
+++ Lecture32: Recursion Day2/Fibbonacci.cpp
@@ -1,10 +1,31 @@
 #include<iostream> 
 using namespace std;
 
+int fibbonacci(int n){
+
+    // Base cases n==0 and n==1
+    
+    if(n==0){
+            return 0;
+        }
+   
+     if(n==1){
+            return 1;
+        }
+     //recursive call    
+   return fibbonacci(n-1)+fibbonacci(n-2);
+ }
 
 int main() {
 
+    int n;
 
+    cout<<"Enter n "<<endl;
+    cin>>n;
+    
+    int ans =fibbonacci(n);
+    
+    System.out.println(ans)
 
 
     return 0;
