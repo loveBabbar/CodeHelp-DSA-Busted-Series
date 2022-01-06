5e2a09144eeb9db64de1215d32c0eb8b074b8bfe
tyagiananya <44269802+tyagiananya@users.noreply.github.com>
2022-01-06 13:36:19 +0530

    Create duplicatesInArray.java

---
+++ Lecture 10: Solving LeetCode Arrays/duplicatesInArray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+    final List<Integer> duplicates = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            final int n = Math.abs(nums[i]);
+            if(nums[n - 1] < 0) {
+                duplicates.add(n);
+            } else {
+                nums[n - 1] *= -1;
+            }
+        }
+        return duplicates;
+        
+    }
+}
