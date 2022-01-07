c78baed99b34ae540981e1bb32730cf787720743
Kishor Khade <85786257+Kishor-khade@users.noreply.github.com>
2022-01-07 15:11:02 +0000

    Lec 20 & 21 code added in both py & cpp

--- Lecture021 Solving Array Questions 2/addArray.cpp
+++ Lecture021 Solving Array Questions 2/addArray.cpp
@@ -1,10 +1,56 @@
 #include<iostream> 
+#include<vector>
 using namespace std; 
 
-int main() {
+
+void reverse(vector<int> &arr){
+    int s = 0, e = arr.size()-1;
+    while (s<=e){
+        swap(arr[s++],arr[e--]);
+    }
+}
 
 
+vector<int> findArraySum(vector<int>&a, int n, vector<int>&b, int m) {
+	// Write your code here.
+    int i = n-1, j = m-1;
+    vector<int>ans;
+    int carry = 0;
+    while (i>=0 && j>=0){
+        int sum = a[i--]+b[j--]+carry;
+		carry = sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+    }
+    
+    while(i>=0){
+        int sum = a[i--]+carry;
+        carry = sum/10;
+        ans.push_back(sum%10);
+    }
+    
+    while(j>=0){
+        int sum = b[j--]+carry;
+        carry = sum/10;
+        ans.push_back(sum%10);
+    }
+    while(carry != 0){
+        ans.push_back(carry%10);
+        carry = carry/10;
+    }
+    reverse(ans);
+    return ans;
+
+}
 
 
+int main(){
+    vector<int>arr1 = {1,2,3,4,5};
+    vector<int>arr2 = {9,8,7,6,0};
+    vector<int>arr3 = findArraySum(arr1, arr1.size(), arr2, arr2.size());
+    cout<<"Final array is ";
+    for (int i:arr3){
+        cout<<i<<' ';
+    }
     return 0;
 }
--- Lecture021 Solving Array Questions 2/checkSortedRotatedArray.cpp
+++ Lecture021 Solving Array Questions 2/checkSortedRotatedArray.cpp
@@ -1,10 +1,20 @@
 #include<iostream> 
+#include<vector>
 using namespace std; 
 
-int main() {
-
-
-
-
+bool check(vector<int>& nums) {
+    int count = 0;
+    int n = nums.size();
+    for (int i=1; i<n; i++){
+        if (nums[i-1] > nums[i]){
+            count++;
+        }
+    }
+    if (nums[n-1] > nums[0]){
+        count++;
+    }
+    if (count <= 1){
+        return 1;
+    }
     return 0;
 }
