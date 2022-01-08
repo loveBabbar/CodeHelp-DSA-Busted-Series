ba99fce38c9b5844908d81739a9252c04d4b92bb
Deep Bag <43214783+deepbag@users.noreply.github.com>
2022-01-08 20:31:37 +0530

    Pyhton Program
    Python program for who want to learn using python

---
+++ Lecture002 Write First C++ Program/pythonprogram.py
@@ -0,0 +1,11 @@
+# In python we have only one print function to print anything
+print("DSA Busted")
+
+# One more example print variable
+a = 10
+print(a)
+
+# One more example print two number sum
+a = 10
+b = 10
+print(a+b)
