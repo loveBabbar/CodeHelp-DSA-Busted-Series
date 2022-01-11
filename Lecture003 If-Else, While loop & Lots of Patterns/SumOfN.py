8637f8bf052ab039a3c3a9be133493bbff5b888b
Tathagata Chakraborty <61090388+CRAZy-Monk3Y@users.noreply.github.com>
2022-01-11 15:48:48 +0530

    Create SumOfN.py
    Added SumOfN in Python language.

---
+++ Lecture003 If-Else, While loop & Lots of Patterns/SumOfN.py
@@ -0,0 +1,12 @@
+# Input variable
+n= int(input())
+
+# defining iteration and sum variable
+itr=1
+sum=0
+
+while(itr<=n):
+  sum = sum +i  # can also be represented--> sum += i
+  i = i+1       # can also be represented--> i += 1
+ 
+print("The value of sum is ",sum) #printing the sum
