254be7e440ade2800c371787295ce48f79c5eaf4
Kaustav Banerjee <35100716+kaustav1810@users.noreply.github.com>
2022-01-11 17:19:48 +0530

    Create reverseString.java

---
+++ Lecture034 Recursion Day4/reverseString.java
@@ -0,0 +1,39 @@
+
+public static void reverse(String str, int i, int j ) {
+    
+    System.out.println("call recieved for "+str);
+
+  //base case
+    if(i>j){
+        System.out.println(name);
+        return ;
+    }
+
+    swap(str,i,j);
+    i++;
+    j--;
+
+    //Recursive call
+    reverse(str,i,j);
+
+}
+
+    public static void swap(String str,int i,int j){
+        int temp;
+        
+        temp = str.charAt(i);
+        str.charAt(i) = str.charAt(j);
+      
+        str.charAt(j) = temp;
+      
+    }
+
+public static void main(String[] args) {
+
+    String name = "abcde";
+    
+    System.out.println(name);
+
+    reverse(name, 0 , name.length()-1 );
+  
+}
