dd0c40aa3de6da0f5f25e0660f1ff1486a4a583f
Kaustav Banerjee <35100716+kaustav1810@users.noreply.github.com>
2022-01-11 17:14:57 +0530

    Create palindromeCheck.java

---
+++ Lecture034 Recursion Day4/palindromeCheck.java
@@ -0,0 +1,29 @@
+
+public static boolean checkPalindrome(String str, int i, int  j) {
+
+    //base case
+    if(i>j)
+        return true;
+
+    if(str.charAt(i) != str.charAt(j))
+        return false;
+    else{
+        //Recursive call
+        return checkPalindrome(str, i+1,j-1);
+    }
+}
+
+public static void main(String[] args) {
+
+    String name = "BookkooB";
+
+    boolean isPalindrome = checkPalindrome(name, 0, name.length()-1 );
+
+    if(isPalindrome) {
+        System.out.println("Its a Palindrome ");
+    }
+    else {
+        System.out.println("Its not a Palindrome " );
+    }
+
+}
