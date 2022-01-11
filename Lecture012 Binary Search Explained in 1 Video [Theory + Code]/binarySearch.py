3f8658ef3606bd1f69f109782fb3255052c3803d
Pranay Gupta <pranayguptamain@gmail.com>
2022-01-11 15:32:01 +0530

    Update and rename Lecture 12: Binary Search Explained in 1 Video [Theory + Code]/binarySearch.py to Lecture012 Binary Search Explained in 1 Video [Theory + Code]/binarySearch.py

--- Lecture012 Binary Search Explained in 1 Video [Theory + Code]/binarySearch.py
+++ Lecture012 Binary Search Explained in 1 Video [Theory + Code]/binarySearch.py
@@ -15,7 +15,6 @@ def binarySearch(arr, size, key):
     mid = start + (end-start)//2
   return -1
 
-
 if __name__ == '__main__':
   even = [2,4,6,8,12,18]
   odd = [3, 8, 11, 14, 16]
@@ -25,4 +24,3 @@ if __name__ == '__main__':
   
   oddIndex = binarySearch(odd, 5, 14)
   print("Index of 14 is: ", oddIndex)
-  
