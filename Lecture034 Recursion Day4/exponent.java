a2c403b9f4a6fe82ddb68d43d4f4721a72660323
Kaustav Banerjee <35100716+kaustav1810@users.noreply.github.com>
2022-01-12 13:33:18 +0530

    Create exponent.java

---
+++ Lecture034 Recursion Day4/exponent.java
@@ -0,0 +1,27 @@
+public static int power(int a, int b) {
+    //base case
+    if( b == 0 )
+        return 1;
+
+    if(b == 1)
+        return a;
+
+    //RECURSIVE CALL
+    int ans = power(a, b/2);
+
+    //if b is even
+    if(b%2 == 0) {
+        return ans * ans;
+    }
+    else {
+        //if b is odd
+        return a * ans * ans;
+    }
+}
+
+public static void main(String[] args) {
+
+    int ans = power(2,5);
+    
+    System.out.println("Answer is " +ans);
+}
