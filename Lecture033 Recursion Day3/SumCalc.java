1a1a494d795adf5490433ab93aae4b05df4e90f0
Pranay Gupta <pranayguptamain@gmail.com>
2022-01-19 20:33:54 +0530

    Update and rename Lecture33: Recursion Day3/SumCalc.java to Lecture033 Recursion Day3/SumCalc.java

--- Lecture033 Recursion Day3/SumCalc.java
+++ Lecture033 Recursion Day3/SumCalc.java
@@ -1,19 +1,14 @@
-package com.company;
-
 public class SumCalc {
     public static void main(String[] args) {
 
         int[] arr = {2 ,4 ,15 ,15 ,15};
-
         int ans = calcSum(arr , 5);
-
         System.out.println(ans); // Ans is 51
     }
 
     public static int calcSum(int[] arr , int size){
         if(size == 1)
             return arr[0];
-
         return sumHelper(arr , 0 , size ,0);
     }
 
@@ -21,9 +16,7 @@ public class SumCalc {
         //base case
         if(idx == size)
             return sum;
-
         int currSum = sum + arr[idx];
-
         return sumHelper(arr , idx + 1 , size , currSum);
     }
 }
