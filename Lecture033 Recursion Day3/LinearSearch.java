ea1b3db3ff06cdfc235e7e0465b82103702f5bfc
Pranay Gupta <pranayguptamain@gmail.com>
2022-01-19 20:33:09 +0530

    Update and rename Lecture33: Recursion Day3/LinearSearch.java to Lecture033 Recursion Day3/LinearSearch.java

--- Lecture033 Recursion Day3/LinearSearch.java
+++ Lecture033 Recursion Day3/LinearSearch.java
@@ -4,12 +4,9 @@ public class LinearSearch {
     public static void main(String[] args) {
 
         int[] arr = {3, 5 ,1 ,2 , 6};
-
         int  ans = search(arr , 7 , 5);
-
         if(ans == -1)
             System.out.println("Not Found");
-
         else
             System.out.println("Element present at index " + ans);
     }
