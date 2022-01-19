7a17017e3380f907bee367e94e181d453a264c24
Pranay Gupta <pranayguptamain@gmail.com>
2022-01-19 20:43:45 +0530

    Update and rename Lecture 4: Solving Pattern Questions (Part-2)/Python Code/pattern9.py to Lecture004 Solving Pattern Questions (Part-2)/Python Code/pattern9.py

--- Lecture004 Solving Pattern Questions (Part-2)/Python Code/pattern9.py
+++ Lecture004 Solving Pattern Questions (Part-2)/Python Code/pattern9.py
@@ -7,14 +7,13 @@ while row <= n:
     while space:
         print(" ", end="")
         space = space - 1
-        
 
     #stars print karlo
     col = 1;
     while col <= row:
         print("*", end="")
         col = col + 1
-       
+
     print()
     row = row + 1
     
