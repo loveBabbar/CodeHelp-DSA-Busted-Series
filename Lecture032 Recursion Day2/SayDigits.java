1d77e88146b3a519e6ca0140d854b51e3e9e84c2
Pranay Gupta <pranayguptamain@gmail.com>
2022-01-19 20:31:45 +0530

    Update and rename Lecture32: Recursion Day2/SayDigit.java to Lecture032 Recursion Day2/SayDigits.java

--- Lecture032 Recursion Day2/SayDigits.java
+++ Lecture032 Recursion Day2/SayDigits.java
@@ -1,7 +1,5 @@
-package com.company;
-
 import java.util.Scanner;
-public class SayDigit {
+public class SayDigits {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
