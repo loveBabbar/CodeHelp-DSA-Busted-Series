1fc54d5c7cbe1de313aa6f5d06d089a668323d32
Pranay Gupta <pranayguptamain@gmail.com>
2022-01-19 20:16:30 +0530

    Update and rename Lecture31: Recursion Day1/Power.java to Lecture031 Recursion Day1/Power.java

--- Lecture031 Recursion Day1/Power.java
+++ Lecture031 Recursion Day1/Power.java
@@ -6,16 +6,13 @@ public class Power {
 		if(n==0) {
 			return 1;
 		}
-		
 		return (2*power(n-1));
 	}
-	
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int power = Power.power(n);
 		System.out.println("2 raise to the power "+n+" is : "+power);
 	}
-
 }
