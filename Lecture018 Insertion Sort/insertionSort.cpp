0ef3f58b6540d8554c7c3f1a53e4312f1458e7ed
Souvik Roy <70591967+Souvik1406@users.noreply.github.com>
2022-01-25 22:05:19 +0530

    Adding Binary Insertion sort to the code
    Adding the highly popular concept of binary insertion sort to the repo.

--- Lecture018 Insertion Sort/insertionSort.cpp
+++ Lecture018 Insertion Sort/insertionSort.cpp
@@ -21,3 +21,45 @@ void insertionSort(int n, vector<int> &arr){
         arr[j+1] = temp;  
     } 
 }
+
+int binarySearch(int a[], int item,
+                 int low, int high)
+{
+    if (high <= low)
+        return (item > a[low]) ?
+                (low + 1) : low;
+ 
+    int mid = (low + high) / 2;
+ 
+    if (item == a[mid])
+        return mid + 1;
+ 
+    if (item > a[mid])
+        return binarySearch(a, item,
+                            mid + 1, high);
+    return binarySearch(a, item, low,
+                        mid - 1);
+}
+ 
+// Function to sort an array a[] of size 'n'
+void BinaryinsertionSort(int a[], int n)
+{
+    int i, loc, j, k, selected;
+ 
+    for (i = 1; i < n; ++i)
+    {
+        j = i - 1;
+        selected = a[i];
+ 
+        // find location where selected should be inseretd
+        loc = binarySearch(a, selected, 0, j);
+ 
+        // Move all elements after location to create space
+        while (j >= loc)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = selected;
+    }
+}
