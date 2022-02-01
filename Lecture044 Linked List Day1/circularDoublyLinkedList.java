075aa2f67742d0e5825739db8419897a9a633a44
Kaustav Banerjee <35100716+kaustav1810@users.noreply.github.com>
2022-02-01 19:02:01 +0530

    Update circularDoublyLinkedList.java

--- Lecture044 Linked List Day1/circularDoublyLinkedList.java
+++ Lecture044 Linked List Day1/circularDoublyLinkedList.java
@@ -1,4 +1,6 @@
-class Node{
+// "static void main" must be defined in a public class.
+public class Main {
+    static class Node{
   int data;
   Node next;
   Node prev;
@@ -11,137 +13,146 @@ class Node{
   }
 }
 
-public void print(Node head){
-  Node curr = head;
-  
-  while(curr.next!=head){
-    System.out.print(curr.data+" ");
-    curr = curr.next;
-  }
-  
-  System.out.println();
+public static void print(Node head){
+  Node temp = head;
+    
+//     if no nodes are present
+    if(temp==null) return;
+    
+    System.out.println();
+    
+  do{
+      System.out.print(temp.data+" ");
+      temp = temp.next;
+  }while(temp!=head);
 }
 
-public Node insertAtHead(Node ptr,int data){
-  Node newNode = new Node(data);
-  
-  if(ptr==null){
-    newNode.next = null;
-    newNode.prev = null;
+public static Node insertAtHead(Node head,int data){
+    Node newNode = new Node(data);
     
-    ptr = newNode;
+    if(head==null){
+        newNode.next = newNode;
+        newNode.prev = newNode;
+        return newNode;
+    }
+    newNode.next = head;
     
-    return ptr;
-  }
-  newNode.next = ptr;
-  ptr.prev = newNode;
-  newNode.prev = ptr;
-  ptr.next = newNode;
-  
-  ptr = newNode;
-  
-  return ptr;
+    Node tail = head.prev;
+    
+    head.prev = newNode;
+    
+    tail.next = newNode;
+    
+    newNode.prev = tail;
+    
+    head = newNode;
+    
+    return head;
 }
 
-public Node insertAtTail(Node ptr,int data){
-  Node newNode = new Node(data);
-  
-  if(ptr==null){
-    newNode.next = null;
-    newNode.prev = null;
+public static Node insertAtTail(Node head,int data){
+    Node newNode = new Node(data);
     
-    ptr = newNode;
+    if(head==null){
+        newNode.next = newNode;
+        newNode.prev = newNode;
+        return newNode;
+    }
+    Node tail = head.prev;
     
-    return ptr;
-  }
-  newNode.next = ptr;
-  
-  ptr.next = newNode;
-  
-  newNode.prev = ptr;
-  
-  ptr.prev = newNode;
-  
-  ptr = newNode;
-  
-  return ptr;
+    tail.next = newNode;
+    
+    newNode.prev = tail;
+    
+    newNode.next = head;
+    
+    head.prev = newNode;
+    
+    tail = newNode;
+    
+    return tail;
+    
 }
 
-public void insertAtPosition(Node head,int pos,int value){
-  Node curr = head,previous = null;
-  
-  if(pos==1){
-    insertAtHead(head,value);
-    return;
-  }
-  
-  if(head.next==null){
-    insertAtTail(head,value);
-    return;
-  }
-  
-  int i = 1;
-  
-  while(i<pos){
-    previous = curr;
-    curr = curr.next;
-    i++;
-  }
-  
-  Node newNode = new Node(value);
-  
-  newNode.next = previous.next;
-  
-  previous.next.prev = newNode;
-  
-  previous.next = newNode;
-  
-  newNode.prev = previous;
+public static void insertAtPosition(Node head,int pos,int value){
+    if(head==null){
+        Node newNode = new Node(value);
+        head = newNode;
+        newNode.next = newNode;
+    }
+    
+    else{
+        int count = 1;
+        
+        Node curr = head;
+        
+        while(count<pos-1){
+            curr = curr.next;
+            count++;
+        }
+        
+        Node newNode= new Node(value);
+        
+        newNode.next = curr.next;
+        
+        curr.next = newNode;
+    }
+    
 }
 
-public void deleteNode(int pos,Node head){
-  Node curr = head,previous = null;
-  
-  if(pos==1){
-    head = null;
-    return;
-  }
- 
-  int i = 1;
-  
-  while(i<pos){
-    previous = curr;
-    curr = curr.next;
-    i++;
-  }
-  
-  previous.next = curr.next;
-  
-  curr.next.prev = previous;
+public static Node deleteNode(int pos,Node head){
+    
+    int i = 1;
+    
+    Node curr = head;
+    
+    while(curr!=null && i<pos-1){
+        
+        curr = curr.next;
+        
+        i++;
+    }
+    
+//   if pos > no. of nodes present
+    if(curr==null) return head;
+    
+    Node deleted = curr.next;
+    
+//     if single node present
+    if(deleted==head){
+        head = null;
+        return head;
+    }
+    curr.next = deleted.next;
+    
+    deleted.next.prev = curr;
+    
+    return head;
 }
 
 public static void main(String[] args) {
     
-    //created a new node
-    Node node1 = new Node(10);
- 
+    Node head = insertAtHead(null,10);
     
-    //head pointed to node1
-    Node head = node1; 
-    
-    print(head);
-
-    head = insertAtTail(head, 12);
+    insertAtTail(head,13);
+    insertAtTail(head,14);
 
     print(head);
 
-    head = insertAtHead(head, 12);
-    print(head);
+//     head = insertAtHead(head, 100);
+//     head = insertAtHead(head, 101);
+//     head = insertAtHead(head, 102);
+//     head = insertAtHead(head, 103);
+//     print(head);
   
-    insertAtPosition(head, 4, 22);
-    print(head);    
+//     insertAtPosition(head, 4, 22);
+//     insertAtPosition(head, 9, 345);
+//     print(head);    
 
-    deleteNode(4, head);
-    print(head);
+//     deleteNode(4, head);
+//     deleteNode(7, head);
+//     deleteNode(2, head);
+//     print(head);
 
+}
 }
