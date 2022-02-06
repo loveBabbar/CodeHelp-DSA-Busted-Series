dc39ce6887a8c5f466fac2fc16edeaa74f9f0a40
guruprasadv22 <guruprasadv22@gmail.com>
2022-02-06 07:01:57 +0530

    lec 7 added desc

--- Lecture007 LeetCode Problem Solving Session/PythonSolutions/powerOfTwo.py
+++ Lecture007 LeetCode Problem Solving Session/PythonSolutions/powerOfTwo.py
@@ -1,3 +1,11 @@
+# Return true if a number can be represented in the powers of two.
+
+# Since 2^31 is the size limit of the input value (integer),
+#     iterate till i == 30
+#         compare 'n' with 'ans' by multiplying 'ans' variable * 2 everytime
+#             return True if equal
+#     else return false after the loop
+    
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         ans = 1
