477b1430cc21467bdc046712e2d5fa2f15f53b80
khushbubijawat <khushbubijawat31@gmail.com>
2022-03-30 10:39:02 +0530

    lecture 48 contribute

--- Lecture048 Linked List Day5/duplicates.cpp
+++ Lecture048 Linked List Day5/duplicates.cpp
@@ -1,43 +1,108 @@
 /************************************************************
 
     Following is the linked list node structure.
-    
-    class Node 
+
+    class Node
     {
         public:
         int data;
         Node* next;
 
-        Node(int data) 
+        Node(int data)
         {
             this->data = data;
             this->next = NULL;
         }
     };
-    
+
 ************************************************************/
 
-Node * uniqueSortedList(Node * head) {
-   	//empty List
-    if(head == NULL)
+Node *uniqueSortedList(Node *head)
+{
+    // empty List
+    if (head == NULL)
         return NULL;
-    
-    //non empty list
-    Node* curr = head;
-    
-    while(curr != NULL) {
-        
-        if( (curr -> next != NULL) && curr -> data == curr -> next -> data) {
-            Node* next_next = curr ->next -> next;
-            Node* nodeToDelete = curr -> next;
-            delete(nodeToDelete);
-            curr -> next = next_next;
-        }
-        else //not equal
+
+    // non empty list
+    Node *curr = head;
+
+    while (curr != NULL)
+    {
+
+        if ((curr->next != NULL) && curr->data == curr->next->data)
         {
-            curr = curr -> next;
-        }   
+            Node *next_next = curr->next->next;
+            Node *nodeToDelete = curr->next;
+            delete (nodeToDelete);
+            curr->next = next_next;
+        }
+        else // not equal
+        {
+            curr = curr->next;
+        }
     }
-    
-    return head; 
+
+    return head;
+}
+
+/* remove duplicates from unsorted linked list method using 2 loops*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A linked list node */
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+// Utility function to create a new Node
+struct Node *newNode(int data)
+{
+    Node *temp = new Node;
+    temp->data = data;
+    temp->next = NULL;
+    return temp;
+}
+
+/* Function to remove duplicates from a
+   unsorted linked list */
+void removeDuplicates(struct Node *start)
+{
+    struct Node *ptr1, *ptr2, *dup;
+    ptr1 = start;
+
+    /* Pick elements one by one */
+    while (ptr1 != NULL && ptr1->next != NULL)
+    {
+        ptr2 = ptr1;
+
+        /* Compare the picked element with rest
+           of the elements */
+        while (ptr2->next != NULL)
+        {
+            /* If duplicate then delete it */
+            if (ptr1->data == ptr2->next->data)
+            {
+                /* sequence of steps is important here */
+                dup = ptr2->next;
+                ptr2->next = ptr2->next->next;
+                delete (dup);
+            }
+            else /* This is tricky */
+                ptr2 = ptr2->next;
+        }
+        ptr1 = ptr1->next;
+    }
+}
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
 }
