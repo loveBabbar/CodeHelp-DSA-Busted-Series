040934ef70e86cb0d8c9dd2c0ba83c6dbb62d69a
Yash Narkhedkar <87686946+yashnarkhedkar@users.noreply.github.com>
2022-06-22 20:27:25 +0530

    Update mergeList.cpp

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -1,3 +1,4 @@
+#include <bits/stdc++.h> 
 /************************************************************
 
     Following is the linked list node structure.
@@ -21,51 +22,58 @@
     };
 
 ************************************************************/
-
-void solve(Node<int>* first, Node<int>* second) {
+Node<int>* solve(Node<int>* first, Node<int>* second){
+    //If a -> next null then attach a to entire b list
+    if(first->next == NULL){
+        first->next = second;
+        return first;
+    }
     
+    Node<int>* curr1 = first;
+    Node<int>* next1 = first -> next;
+    Node<int>* curr2 = second;
+    Node<int>* next2 = second -> next;
     
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
-    
-    while(next1 != NULL && curr2 != NULL) {
-        
-        if( (curr2 -> data >= curr1 -> data ) 
-           && ( curr2 -> data <= next1 -> data)) {
-            
-            curr1 -> next = curr2;
+    while(next1 != NULL && curr2 != NULL){
+        if((curr2->data >= curr1->data) 
+           && (curr2->data <= next1->data)){
+            curr1->next = curr2;
+            next2 = curr2 -> next;
             curr2 -> next = next1;
+            
+            //Update Pointers
             curr1 = curr2;
             curr2 = next2;
         }
-        else {
-            
+        else{
+            //Update Pointers
+            curr1 = next1;
+            next1 = next1 -> next;
+            if(next1 == NULL){
+                curr1 -> next = curr2;
+                return first;
+            }
         }
-        
-        
     }
-    
-    
+    return first;
 }
 
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
 {
-    if(first == NULL)
+    // Write your code here.
+    if(first == NULL){
         return second;
+    }
     
-    if(second == NULL)
+    if(second == NULL){
         return first;
-    
-    if(first -> data <= second -> data ){
-        solve(first, second);
-    }
-    else
-    {
-        solve(second, first);
     }
     
+    if(first->data <= second->data){
+        return solve(first, second);
+    }
     
+    else{
+        return solve(second, first);
+    }
 }
