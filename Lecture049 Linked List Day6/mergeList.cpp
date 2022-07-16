201a0387674e96bf02979b52fbd9492f37839971
VjManish <91712461+VjManish@users.noreply.github.com>
2022-07-16 17:50:31 +0530

    Update mergeList.cpp

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -22,9 +22,12 @@
 
 ************************************************************/
 
-void solve(Node<int>* first, Node<int>* second) {
+Node<int>*  solve(Node<int>* first, Node<int>* second) {
     
-    
+    if(first->next==NULL) {
+        first->next=second;
+        return first;
+    }
     Node* curr1 = first;
     Node* next1 = curr1 -> next;
     
@@ -42,13 +45,16 @@ void solve(Node<int>* first, Node<int>* second) {
             curr2 = next2;
         }
         else {
+            curr1= next;
+            next1= next1->next;
             
-        }
-        
-        
+            if(next1==NULL) {
+              curr1->next=curr2;
+                return first;
+            }
     }
     
-    
+    return first;
 }
 
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
@@ -60,11 +66,11 @@ Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
         return first;
     
     if(first -> data <= second -> data ){
-        solve(first, second);
+      return  solve(first, second);
     }
     else
     {
-        solve(second, first);
+      return   solve(second, first);
     }
     
     
