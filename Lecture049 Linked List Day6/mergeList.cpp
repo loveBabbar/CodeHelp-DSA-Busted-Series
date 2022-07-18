75446aeda402da9707f5cf61e31b51bddab9ce3a
Vishal Yadav <85288372+vishal-y@users.noreply.github.com>
2022-07-18 18:25:13 +0530

    Update mergeList.cpp

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -22,33 +22,43 @@
 
 ************************************************************/
 
-void solve(Node<int>* first, Node<int>* second) {
+Node<int>* solve(Node<int>* first, Node<int>* second) {
     
     
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
+    // if only one element is present in first line.
+    if(first->next == NULL){
+        first->next = second;
+        return first;
+    }
+
+    Node<int>* curr1 = first;
+    Node<int>* next1 = curr1->next;
+    Node<int>* curr2 = second;
+    Node<int>* next2 = curr2;
     
     while(next1 != NULL && curr2 != NULL) {
         
         if( (curr2 -> data >= curr1 -> data ) 
            && ( curr2 -> data <= next1 -> data)) {
-            
+            // add node in between the first list
             curr1 -> next = curr2;
             curr2 -> next = next1;
+            next2 = curr2->next;
+            // update pointers
             curr1 = curr2;
             curr2 = next2;
         }
         else {
-            
+            // curr1 and next1 ko aage badhana hoga
+            curr1 = next1;
+            next1 = next1->next;
+            if(next1==NULL){
+                curr1->next = curr2;
+                return first;
+            }
         }
-        
-        
     }
-    
-    
+    return first;
 }
 
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
@@ -60,12 +70,11 @@ Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
         return first;
     
     if(first -> data <= second -> data ){
-        solve(first, second);
+        return solve(first, second);
     }
     else
     {
-        solve(second, first);
+        return solve(second, first);
     }
-    
-    
+      
 }
