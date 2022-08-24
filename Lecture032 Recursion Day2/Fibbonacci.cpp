e9bc0c96ee10de390add567cf42c9cf45783831a
Shanukumar Singh <91169674+Shanu2409@users.noreply.github.com>
2022-08-24 18:19:05 +0530

    code added....

--- Lecture032 Recursion Day2/Fibbonacci.cpp
+++ Lecture032 Recursion Day2/Fibbonacci.cpp
@@ -1,10 +1,18 @@
 #include<iostream> 
 using namespace std;
 
+int fibo(int n){
+    if (n == 0 || n == 1) return n;
+    return fibo(n-1) + fibo(n-2);
+}
 
 int main() {
 
-
+    int n;
+    cout<<"Enter number : "<<endl;
+    cin>>n;
+    
+    cout<<fibo(n)<<endl;
 
 
     return 0;
