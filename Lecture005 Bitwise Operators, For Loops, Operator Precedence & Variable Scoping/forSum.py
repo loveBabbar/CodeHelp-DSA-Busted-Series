9d888c71ed5408d603b8c28e109b395c38820989
Pratham Jaiswal <85238761+pprathamjaiswal@users.noreply.github.com>
2022-10-27 01:51:15 +0530

    for Sum

---
+++ Lecture005 Bitwise Operators, For Loops, Operator Precedence & Variable Scoping/forSum.py
@@ -0,0 +1,13 @@
+# Program to find the sum of all numbers stored in a list
+
+# List of numbers
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+# variable to store the sum
+sum = 0
+
+# iterate over the list
+for val in numbers:
+    sum = sum+val
+
+print("The sum is", sum)
