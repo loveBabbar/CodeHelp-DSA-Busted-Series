5ec336368c4deac6e4f57695782a8e9defd54245
Yuvraj Singh Yadav <30363687+yyuvraj54@users.noreply.github.com>
2022-10-29 09:21:24 +0530

    file updated
    All Test Cases are been passed by this code.
    Added comment for better understanding.

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -1,3 +1,4 @@
+#include <bits/stdc++.h> 
 /************************************************************
 
     Following is the linked list node structure.
@@ -21,51 +22,66 @@
     };
 
 ************************************************************/
-
-void solve(Node<int>* first, Node<int>* second) {
+Node<int>* MergeTwoSorted_LL(Node<int>* first,Node<int>* second){
     
-    
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
-    
-    while(next1 != NULL && curr2 != NULL) {
-        
-        if( (curr2 -> data >= curr1 -> data ) 
-           && ( curr2 -> data <= next1 -> data)) {
-            
-            curr1 -> next = curr2;
-            curr2 -> next = next1;
-            curr1 = curr2;
-            curr2 = next2;
-        }
-        else {
-            
-        }
-        
-        
+    //If 1st Linked List contains only one element then it next will be pointed to 2nd Linked List
+    if(first->next==NULL){
+        first->next=second;
+        return first;
     }
-    
-    
-}
 
+
+    Node<int>* curr1= first;
+    Node<int>* next1=curr1->next;
+    Node<int>* curr2=second;
+    Node<int>* next2=curr2->next;
+    
+
+     while(next1!=NULL && curr2!=NULL){
+
+       if((curr2->data<=next1->data) && (next1->data>=curr2->data)){
+        
+        // Inserting the node in the Linked List
+        curr1->next=curr2;
+        next2=curr2->next;
+        curr2->next=next1;
+           
+        // updating pointers
+        curr1=curr2;
+        curr2=next2; 
+       
+       }
+       else{
+           
+           //If Element can't be inserted we will move the pointer to next nodes   
+            curr1=next1;
+            next1=next1->next;
+           
+         // If next for 1st LL node reach NULL
+         if (next1==NULL){
+            curr1->next=curr2;
+            return first;
+            }
+        }
+    }
+
+     return first;
+}
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
 {
-    if(first == NULL)
+    //Checking if any of the Linked list is NULL
+    if(first==NULL){
         return second;
-    
-    if(second == NULL)
+    }
+    if(second==NULL){
         return first;
-    
-    if(first -> data <= second -> data ){
-        solve(first, second);
-    }
-    else
-    {
-        solve(second, first);
     }
     
     
+    if(first->data<=second->data){
+            return MergeTwoSorted_LL(first , second);
+        }
+    else{
+           return MergeTwoSorted_LL(second , first);
+        }
 }
