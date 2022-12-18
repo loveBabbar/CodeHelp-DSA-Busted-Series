5377d48afcebd358ad46074f2a321f34aa9d0eeb
Pratyush Raj <72189926+pratyush2331@users.noreply.github.com>
2022-12-19 01:42:43 +0530

    Update mergeList.cpp
    solve() function should be of Node<int>* type and it should return the node. ANd other some necessary changes are done.

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -22,50 +22,54 @@
 
 ************************************************************/
 
-void solve(Node<int>* first, Node<int>* second) {
+Node<int>* solve(Node<int>* first, Node<int>* second) {
+    // if only one element is present in first list
+    if(first->next == NULL) {
+        first->next = second;
+        return first;
+    }
     
-    
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
+    Node<int>* curr1 = first;
+    Node<int>* next1 = curr1->next;
+    Node<int>* curr2 = second;
+    Node<int>* next2 = curr2->next;
     
     while(next1 != NULL && curr2 != NULL) {
-        
-        if( (curr2 -> data >= curr1 -> data ) 
-           && ( curr2 -> data <= next1 -> data)) {
+        if(curr2->data >= curr1->data && curr2->data <= next1->data) {
+            curr1->next = curr2;
+            next2 = curr2->next;
+            curr2->next = next1;
             
-            curr1 -> next = curr2;
-            curr2 -> next = next1;
             curr1 = curr2;
             curr2 = next2;
         }
         else {
+            curr1 = next1;
+            next1 = next1->next;
             
+            if(next1 == NULL) {
+                curr1->next = curr2;
+                return first;
+            }
         }
-        
-        
     }
     
-    
+    return first;
 }
 
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
 {
-    if(first == NULL)
+    if(first == NULL) {
         return second;
-    
-    if(second == NULL)
+    }
+    if(second == NULL) {
         return first;
-    
-    if(first -> data <= second -> data ){
-        solve(first, second);
-    }
-    else
-    {
-        solve(second, first);
     }
     
-    
+     if(first->data <= second->data) {
+         return solve(first, second);
+     }
+    else {
+         return solve(second, first);
+    }
 }
