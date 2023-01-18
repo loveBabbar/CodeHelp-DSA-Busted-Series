ece0a32f8b2e6fe66e44d5e4ed2680c972fe1910
Burhanuddin S. Tinwala <113278150+BST5319@users.noreply.github.com>
2023-01-18 23:36:02 +0530

    Update mergeList.cpp

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -23,13 +23,16 @@
 ************************************************************/
 
 void solve(Node<int>* first, Node<int>* second) {
+    if(first -> next == NULL){
+        first -> next = second;
+        return first;
+    }
     
+    Node<int>* curr1 = first;
+    Node<int>* next1 = curr1 -> next;
     
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
+    Node<int>* curr2 = second;
+    Node<int>* next2 = curr2 -> next;
     
     while(next1 != NULL && curr2 != NULL) {
         
@@ -37,18 +40,22 @@ void solve(Node<int>* first, Node<int>* second) {
            && ( curr2 -> data <= next1 -> data)) {
             
             curr1 -> next = curr2;
+            next2 = curr2 -> next;
             curr2 -> next = next1;
             curr1 = curr2;
             curr2 = next2;
         }
         else {
-            
-        }
-        
-        
+            curr1 =next1;
+            next1 = next1 -> next;
+            if(next1 == NULL)
+            {
+                curr1->next = curr2;
+                return first;
+            }
+        } 
     }
-    
-    
+    return first;
 }
 
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
