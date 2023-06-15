06591e66169bbd514fef95227346cf2f3c30efd8
Shanukumar Singh <91169674+Shanu2409@users.noreply.github.com>
2023-06-15 16:27:02 +0530

    Update Fibbonacci.cpp

--- Lecture032 Recursion Day2/Fibbonacci.cpp
+++ Lecture032 Recursion Day2/Fibbonacci.cpp
@@ -1,11 +1,18 @@
-#include<iostream> 
+#include <iostream>
 using namespace std;
 
+int fib(int n){
+	if (n == 0)
+		return 0;		
+	if (n == 1)
+		return 1;
 
-int main() {
+	int ans = fib(n-1) + fib(n-2);	
+	return ans;
+}
 
-
-
-
-    return 0;
+int main()
+{
+	cout<<fib(3);
+	return 0;
 }
