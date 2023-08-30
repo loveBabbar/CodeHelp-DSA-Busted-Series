321ddc49e925a8a56153ebe02154e4a5cd7af41b
krishnagumaste <76817233+krishnagumaste@users.noreply.github.com>
2023-08-30 14:32:06 +0530

    Update mergeList.cpp
    Completed the rest of the merge sorted list code works fine in code studio for "Merge Two Sorted Linked Lists" problem.

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -22,14 +22,18 @@
 
 ************************************************************/
 
-void solve(Node<int>* first, Node<int>* second) {
+Node<int>* solve(Node<int>* first, Node<int>* second) {
+
+    if(first -> next == NULL){
+        first -> next = second;
+        return first;
+    }
     
+    Node<int>* curr1 = first;
+    Node<int>* next1 = curr1 -> next;
     
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
+    Node<int>* curr2 = second;
+    Node<int>* next2 = curr2 -> next;
     
     while(next1 != NULL && curr2 != NULL) {
         
@@ -37,18 +41,23 @@ void solve(Node<int>* first, Node<int>* second) {
            && ( curr2 -> data <= next1 -> data)) {
             
             curr1 -> next = curr2;
+            next2 = curr2 -> next;
             curr2 -> next = next1;
+
             curr1 = curr2;
             curr2 = next2;
         }
-        else {
-            
+        else{
+            curr1 = curr1 -> next;
+            next1 = next1 -> next;
+            if(next1==NULL){
+                curr1 -> next = curr2;
+                return first;
+            }
         }
-        
-        
     }
     
-    
+    return first;
 }
 
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
@@ -60,12 +69,10 @@ Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
         return first;
     
     if(first -> data <= second -> data ){
-        solve(first, second);
+        return solve(first, second);
     }
     else
     {
-        solve(second, first);
+        return solve(second, first);
     }
-    
-    
 }
