ad6c1082416d595ac4d138f1e73b2dc567d4e8e3
pratik joshi <83777309+pratikrj1601@users.noreply.github.com>
2023-10-04 18:21:34 +0530

    Create pattern_21.cpp

---
+++ Lecture004 Solving Pattern Questions (Part-2)/c++ Patterns/pattern_21.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+// Printing patterns using conditionals and if loops
+
+// 4 3 2 1
+// 3 2 1
+// 2 1 
+// 1
+
+/* Approach: we are observing that row number starts from 1 to n (n is an integer value given by user) and in each row there is n-i+1 number of columns.
+// For Ex. first row has 4 column, 2nd row has 3 columns, 3rd has 2 columns and so on.... and in each row we are printing value of n-j+1. */
+
+int main() {
+
+    int n, i=1;
+    cin>>n;
+
+    while (i<=n) {
+
+        int j = 1;        
+        while (j<=n-i+1) {
+            cout<<n-j+1<<" "; 
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+}
