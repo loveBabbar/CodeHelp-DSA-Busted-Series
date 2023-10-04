2d13c7abb495df9684fc1781a169f380479540c1
pratik joshi <83777309+pratikrj1601@users.noreply.github.com>
2023-10-04 18:30:16 +0530

    Create pattern_41.cpp

---
+++ Lecture004 Solving Pattern Questions (Part-2)/c++ Patterns/pattern_41.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+// E E E E E 
+//   D D D D 
+//     C C C 
+//       B B 
+//         A 
+
+int main() {
+
+    int n, i=1;
+    cin>>n;
+ 
+    while (i<=n) {
+        int space = i-1;
+        while(space) {
+            cout<<"  ";
+            space--;
+        }
+
+        int j = 1;
+        while (j<=n-i+1) {
+            cout<<char(n-i+'A')<<" "; 
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+}
