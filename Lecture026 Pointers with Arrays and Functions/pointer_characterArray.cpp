a0d1c8505f408985097f117652242067835c5fe0
Abhishek Jaiswal <98872450+Abhijais2003@users.noreply.github.com>
2023-11-04 01:26:54 +0530

    Update pointer_characterArray.cpp

--- Lecture026 Pointers with Arrays and Functions/pointer_characterArray.cpp
+++ Lecture026 Pointers with Arrays and Functions/pointer_characterArray.cpp
@@ -1,25 +1,36 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main() {
+int main()
+{
+    int arr[3] = {1, 2, 3};
+    char ch[4] = "abc";
 
-    int arr[5] = {1,2,3,4,5};
-    char ch[6] = "abcde";
-
+    // cout works different in case of character array as compare to Integer array
 
     cout << arr << endl;
-    //attention here
     cout << ch << endl;
-    
+
     char *c = &ch[0];
-    //prints entire string
+
+    // Prints entire character array
     cout << c << endl;
 
-    char temp = 'z';
+    // This prints the values
+    cout << *c << endl;
+
+    // If there is no NUll values
+
+    char temp = 'm';
+
     char *p = &temp;
 
+    // Prints until it finds the Null character '\0'
     cout << p << endl;
 
+    // Print the actual values
+    cout << *p << endl;
 
     return 0;
 }
